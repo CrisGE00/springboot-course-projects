eb926a23f5260212daa352892a45af71414d5e76
CrisGE00 <crisge10@hotmail.es>
2024-09-05 12:42:16 +0200

    Wiring with method parameters

--- beansWiringExample/src/main/java/com/crisgeproject/config/ProjectConfig.java
+++ beansWiringExample/src/main/java/com/crisgeproject/config/ProjectConfig.java
@@ -16,11 +16,22 @@ public class ProjectConfig {
 		return vehicle;
 	}
 	
+	/** Wiring without method parameters
+	
 	@Bean
 	public Person person() {
 		Person person = new Person();
 		person.setName("Lucy");
 		person.setVehicle(vehicle());
+		return person;
+	} **/
+	
+	// Wiring with method parameters
+	@Bean
+	public Person person(Vehicle vehicle) {
+		Person person = new Person();
+		person.setName("Lucy");
+		person.setVehicle(vehicle);
 		return person;
 	}
 }
