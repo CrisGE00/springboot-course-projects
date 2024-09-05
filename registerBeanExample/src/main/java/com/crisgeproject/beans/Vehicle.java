ad2720e68e251462f97e157fae439b1a47894284
CrisGE00 <crisge10@hotmail.es>
2024-09-05 10:27:50 +0200

    Project creation

---
+++ registerBeanExample/src/main/java/com/crisgeproject/beans/Vehicle.java
@@ -0,0 +1,19 @@
+package com.crisgeproject.beans;
+
+public class Vehicle {
+
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void printHello(){
+		System.out.println("Helloooo");
+	}
+	
+}
---
+++ registerBeanExample/src/main/java/com/crisgeproject/config/ProjectConfig.java
@@ -0,0 +1,10 @@
+package com.crisgeproject.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.crisgeproject.beans")
+public class ProjectConfig {
+
+}
