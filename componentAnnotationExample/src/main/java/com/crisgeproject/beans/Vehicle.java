6fe597ece1a70ee507495d0dae48c11bff12f6e6
CrisGE00 <crisge10@hotmail.es>
2024-09-05 10:18:57 +0200

    PreDestroy annotation
    Added PreDestroy annotation

--- componentAnnotationExample/src/main/java/com/crisgeproject/beans/Vehicle.java
+++ componentAnnotationExample/src/main/java/com/crisgeproject/beans/Vehicle.java
@@ -3,6 +3,7 @@ package com.crisgeproject.beans;
 import org.springframework.stereotype.Component;
 
 import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 
 @Component
 public class Vehicle {
@@ -20,6 +21,11 @@ public class Vehicle {
 	@PostConstruct
 	public void initialize() {
 		this.name = "Ford";
+	}
+	
+	@PreDestroy
+	public void destroy() {
+		System.out.println("Destroying bean...");
 	}
 	
 	public void printHello(){
