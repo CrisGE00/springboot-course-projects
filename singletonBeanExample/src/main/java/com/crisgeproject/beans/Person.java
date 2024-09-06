c7e0c1381a8000113cd33cc87b75ae00a721ce9d
CrisGE00 <crisge10@hotmail.es>
2024-09-06 11:00:00 +0200

    Singleton bean example

---
+++ singletonBeanExample/src/main/java/com/crisgeproject/beans/Person.java
@@ -0,0 +1,37 @@
+package com.crisgeproject.beans;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import jakarta.annotation.PostConstruct;
+
+@Component
+public class Person {
+
+	private String name;
+	private final Vehicle vehicle;
+	
+	@Autowired
+	public Person(Vehicle vehicle) {
+		this.vehicle = vehicle;
+	}
+	
+	@PostConstruct
+	public void initialize() {
+		this.name = "Lucy";
+	}
+	
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Vehicle getVehicle() {
+		return vehicle;
+	}	
+	
+}
---
+++ singletonBeanExample/src/main/java/com/crisgeproject/singletonBeanExample/App.java
@@ -0,0 +1,24 @@
+package com.crisgeproject.singletonBeanExample;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.crisgeproject.config.ProjectConfig;
+import com.crisgeproject.services.VehicleService;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+        VehicleService vehicleService1 = context.getBean(VehicleService.class);
+        VehicleService vehicleService2 = context.getBean("vehicleService", VehicleService.class);
+        System.out.println("Hashcode of vehicleservice1: " + vehicleService1.hashCode());
+        System.out.println("Hashcode of vehicleservice2: " + vehicleService2.hashCode());
+        
+        if(vehicleService1 == vehicleService2) {
+        	System.out.println("VehicleServices bean is a singleton scoped bean");
+        }
+        
+        context.close();
+    }
+}
