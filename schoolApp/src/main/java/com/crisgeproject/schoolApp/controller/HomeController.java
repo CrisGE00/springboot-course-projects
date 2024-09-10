9501ca30a467e0f440244fbaf3d195d89363d42d
CrisGE00 <crisge10@hotmail.es>
2024-09-10 15:45:09 +0200

    Various changes
    - Added Springboot DevTools dependency
    - Moved home.html to templates folder
    - Modified application.properties

--- schoolApp/src/main/java/com/crisgeproject/schoolApp/controller/HomeController.java
+++ schoolApp/src/main/java/com/crisgeproject/schoolApp/controller/HomeController.java
@@ -1,13 +1,15 @@
 package com.crisgeproject.schoolApp.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
 	@RequestMapping({"", "/", "/home"})
-	public String displayHomePage() {
+	public String displayHomePage(Model model) {
+		model.addAttribute("username", "Mary Sue");
 		return "home.html";
 	}
 }
