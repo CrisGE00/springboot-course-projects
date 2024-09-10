b080446226b39a9c54d119405e0f1e9956ee9f13
CrisGE00 <crisge10@hotmail.es>
2024-09-10 12:04:12 +0200

    Project Creation
    Spring MVC project creation

---
+++ schoolApp/src/test/java/com/crisgeproject/schoolApp/SchoolAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crisgeproject.schoolApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
