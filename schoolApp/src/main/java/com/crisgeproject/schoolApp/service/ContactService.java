a79913dd815a773c8285e604741dfb18e288941e
CrisGE00 <crisge10@hotmail.es>
2024-09-12 14:18:52 +0200

    Added Slf4j annotation

--- schoolApp/src/main/java/com/crisgeproject/schoolApp/service/ContactService.java
+++ schoolApp/src/main/java/com/crisgeproject/schoolApp/service/ContactService.java
@@ -4,12 +4,15 @@ import org.springframework.stereotype.Service;
 
 import com.crisgeproject.schoolApp.model.Contact;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Service
 public class ContactService {
 	
 	public boolean saveMessageDetails(Contact contact) {
 		boolean isSaved = true;
-		System.out.println(contact.toString());
+		log.info(contact.toString());
 		return isSaved;
 	}
 }
