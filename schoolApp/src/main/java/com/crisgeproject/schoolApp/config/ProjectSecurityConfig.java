76507305dfe1c039f26770430aa311ea7f7ec1c8
CrisGE00 <crisge10@hotmail.es>
2024-09-18 15:22:06 +0200

    Migrated H2 database to AWS

--- schoolApp/src/main/java/com/crisgeproject/schoolApp/config/ProjectSecurityConfig.java
+++ schoolApp/src/main/java/com/crisgeproject/schoolApp/config/ProjectSecurityConfig.java
@@ -1,6 +1,5 @@
 package com.crisgeproject.schoolApp.config;
 
-import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -16,7 +15,7 @@ public class ProjectSecurityConfig {
 	@Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
 
-        http.csrf((csrf) -> csrf.ignoringRequestMatchers("/saveMsg").ignoringRequestMatchers(PathRequest.toH2Console()))
+        http.csrf((csrf) -> csrf.ignoringRequestMatchers("/saveMsg"))
         .authorizeHttpRequests(requests -> requests.requestMatchers("/", "/home").permitAll()
         		.requestMatchers("/displayMessages").hasRole("ADMIN")
                 .requestMatchers("/closeMsg/**").hasRole("ADMIN")
@@ -29,7 +28,6 @@ public class ProjectSecurityConfig {
                 .requestMatchers("/login").permitAll()
                 .requestMatchers("/logout").permitAll()
                 .requestMatchers("/logout").permitAll()
-                .requestMatchers(PathRequest.toH2Console()).permitAll()
                 .requestMatchers("/dashboard").authenticated())
 		        .formLogin(loginConfigurer -> loginConfigurer.loginPage("/login")
 		                .defaultSuccessUrl("/dashboard").failureUrl("/login?error=true").permitAll())
@@ -37,8 +35,6 @@ public class ProjectSecurityConfig {
 		                .invalidateHttpSession(true).permitAll())
 		        .httpBasic(Customizer.withDefaults());
 
-        http.headers(headersConfigurer -> headersConfigurer
-                .frameOptions(frameOptionsConfig -> frameOptionsConfig.disable()));
         return http.build();
 
     }
--- schoolApp/src/main/resources/schema.sql
+++ schoolApp/src/main/resources/schema.sql
@@ -1,3 +1,7 @@
+create database schoolapp;
+
+use schoolapp;
+
 CREATE TABLE IF NOT EXISTS `contact_msg` (
   `contact_id` int AUTO_INCREMENT  PRIMARY KEY,
   `name` varchar(100) NOT NULL,
