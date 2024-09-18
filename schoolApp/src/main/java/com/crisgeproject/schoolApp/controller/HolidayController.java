d10cf9e898f1281f02efecca33a2fd2f38bd5c86
CrisGE00 <crisge10@hotmail.es>
2024-09-18 12:58:37 +0200

    Moved hardcoded holiday list to H2 database

--- schoolApp/src/main/java/com/crisgeproject/schoolApp/controller/HolidayController.java
+++ schoolApp/src/main/java/com/crisgeproject/schoolApp/controller/HolidayController.java
@@ -1,18 +1,22 @@
 package com.crisgeproject.schoolApp.controller;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import com.crisgeproject.schoolApp.model.Holiday;
+import com.crisgeproject.schoolApp.repository.HolidaysRepository;
 
 @Controller
 public class HolidayController {
+	
+	@Autowired
+	private HolidaysRepository holidaysRepository;
 
 	@GetMapping("/holidays/{display}")
     public String displayHolidays(@PathVariable String display, Model model) {
@@ -24,16 +28,7 @@ public class HolidayController {
 		}else if(null != display && display.equals("festival")) {
 			model.addAttribute("festival", true);
 		}
-        List<Holiday> holidays = Arrays.asList(
-                new Holiday(" Jan 1 ","New Year's Day", Holiday.Type.FESTIVAL),
-                new Holiday(" Oct 31 ","Halloween", Holiday.Type.FESTIVAL),
-                new Holiday(" Nov 24 ","Thanksgiving Day", Holiday.Type.FESTIVAL),
-                new Holiday(" Dec 25 ","Christmas", Holiday.Type.FESTIVAL),
-                new Holiday(" Jan 17 ","Martin Luther King Jr. Day", Holiday.Type.FEDERAL),
-                new Holiday(" July 4 ","Independence Day", Holiday.Type.FEDERAL),
-                new Holiday(" Sep 5 ","Labor Day", Holiday.Type.FEDERAL),
-                new Holiday(" Nov 11 ","Veterans Day", Holiday.Type.FEDERAL)
-        );
+        List<Holiday> holidays = holidaysRepository.findAllHolidays();
         Holiday.Type[] types = Holiday.Type.values();
         for (Holiday.Type type : types) {
             model.addAttribute(type.toString(),
